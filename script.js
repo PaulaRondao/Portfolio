c6e03758d9a36c89d58c5fb6344bc559bf95228a
PaulaRondao <134066088+PaulaRondao@users.noreply.github.com>
2023-11-24 11:56:27 +0100

    First update

---
+++ script.js
@@ -0,0 +1,14 @@
+//Nav-bar : to keep a link active
+
+// function clickSingleA(a)
+// {
+//     items = document.querySelectorAll('.nav-link.active');
+
+//     if(items.length) 
+//     {
+//         items[0].className = 'nav-link';
+//     }
+
+//     a.className = 'nav-link active';
+// }
+
