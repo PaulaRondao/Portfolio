8b0cb13b1175dc6b1f00708384d616d7cf550398
PaulaRondao <maud.rondao@gmail.com>
2024-01-25 12:32:00 +0100

    Add button scroll top

--- scr/main.js
+++ scr/main.js
@@ -59,4 +59,33 @@ function includeFooterHTML() {
 }
 includeFooterHTML()
 
+// Button scroll top
+const displayButton = () => {
+  window.addEventListener('scroll', () => {
+    console.log(window.scrollY);
+  
+    if (window.scrollY > 100) {
+      let buttonTop = document.querySelector(".btn-top"); 
+      buttonTop.style.display = "block";
+    } else {
+      let buttonTop = document.querySelector(".btn-top"); 
+      buttonTop.style.display = "none";
+    }
+  });
+};
+
+const scrollToTop = () => {
+  let buttonTop = document.querySelector(".btn-top"); 
+  buttonTop.addEventListener("click", () => {
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
+    }); 
+    console.log(event);
+  });
+};
+
+displayButton();
+scrollToTop();
 
