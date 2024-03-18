014cfa7f5c9e497aab0ed4669c9ca6c0ccf04f54
PaulaRondao <maud.rondao@gmail.com>
2024-03-18 22:32:48 +0100

    mise a jour pour le SEO et indexation Google

--- scr/app.js
+++ scr/app.js
@@ -1,7 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
     var scrollToTopBtn = document.getElementById("scrollToTopBtn");
 
-    // Show or hide the button based on scroll position
     window.addEventListener("scroll", function () {
         if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
             scrollToTopBtn.style.display = "block";
@@ -10,10 +9,9 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 
-    // Scroll to the top when the button is clicked
     scrollToTopBtn.addEventListener("click", function () {
-        document.body.scrollTop = 0; // For Safari
-        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
     });
 });
 
--- scr/main.js
+++ scr/main.js
@@ -1,4 +1,3 @@
-// menu burger
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 const openText = document.querySelector("#open-text");
@@ -43,30 +42,24 @@ function closeMenu() {
 
 
 
-// Include footer html
 function includeFooterHTML() {
   let z, i, elmnt, file, xhttp;
-  /* Loop through a collection of all HTML elements: */
   z = document.getElementsByTagName("*");
   for (i = 0; i < z.length; i++) {
     elmnt = z[i];
-    /*search for elements with a certain atrribute:*/
     file = elmnt.getAttribute("footer-include-html");
     if (file) {
-      /* Make an HTTP request using the attribute value as the file name: */
       xhttp = new XMLHttpRequest();
       xhttp.onreadystatechange = function () {
         if (this.readyState == 4) {
           if (this.status == 200) { elmnt.innerHTML = this.responseText; }
           if (this.status == 404) { elmnt.innerHTML = "Page not found."; }
-          /* Remove the attribute, and call this function once more: */
           elmnt.removeAttribute("footer-include-html");
           includeFooterHTML();
         }
       }
       xhttp.open("GET", file, true);
       xhttp.send();
-      /* Exit the function: */
       return;
     }
   }
